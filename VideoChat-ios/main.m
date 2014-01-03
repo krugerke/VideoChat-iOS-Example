cc538fdcfb5ea7dd43b050f59996a7c86c3869eb
Yury Yaschenko <yura.yaschenko@gmail.com>
2014-01-03 18:56:15 +0200

    Initial Commit

---
+++ VideoChat-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoChat-ios
+//
+//  Created by Yury Yaschenko on 1/3/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
